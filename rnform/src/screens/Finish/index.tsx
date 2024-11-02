d1c0e202113000cb41a4c4afe0c6de3c5b52ae45
Roberto Dos Santos Filho <robertodossantosfilho1001@gmail.com>
2024-11-02 02:27:41 +0000

    biblioteca de confeti ao finalizar formulário

--- rnform/src/screens/Finish/index.tsx
+++ rnform/src/screens/Finish/index.tsx
@@ -1,10 +1,12 @@
 import { Text, View } from "react-native";
 import { useAccountForm } from "../../hooks/useAccountForm";
+import {Confetti} from "react-native-fast-confetti"
 
 export function Finish(){
     const {accountFormData}  = useAccountForm()
     return(
         <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+            <Confetti />
             <Text>
                 {accountFormData.name}
             </Text>
